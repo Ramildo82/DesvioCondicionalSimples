c8eddce0252ade0da39340dde5841ee0faf9a7a7
Ramildo82 <ramildo82@gmail.com>
2021-11-06 13:33:57 -0300

    Desvio Condicional Simples

---
+++ IdadeJogador/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace IdadeJogador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte idade;
+
+            Console.WriteLine("Digite sua idade: ");
+            idade = Convert.ToByte(Console.ReadLine());
+
+            if (idade >= 12)
+            {
+                Console.WriteLine("Você pode jogar!");
+            }
+
+        }
+    }
+}
